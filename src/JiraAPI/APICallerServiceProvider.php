a4bdf472f97843d513239cba43a89e69795e594e
DeRidStijn <deridder.stijn93@gmail.com>
2018-05-04 11:21:29 +0200

    my first commit

---
+++ src/JiraAPI/APICallerServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace JiraAPI;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use GuzzleHttp\Client;
+
+class APICallerServiceProvider
+{
+    /*If you have a url and your php supports it, you could just call file_get_contents:
+
+    $response = file_get_contents('http://example.com/path/to/api/call?param1=5');
+    if $response is JSON, use json_decode to turn it into php array:
+
+    $response = json_decode($response);
+    if $response is XML, use simple_xml class:
+
+    $response = new SimpleXMLElement($response);*/
+
+
+
+    public function getClearfactsSprint()
+    {
+        /**
+         * Resource: Jira is considered the resource
+         * Resource owner: Jira user
+         * Consumer: User of the application
+         * http://jira.tactics.be:8080/rest/agile/latest/board/1
+         */
+
+        $client = new Client();
+        $response = $client->get('http://jira.tactics.be:8080/rest/agile/latest/board/1', [
+            'auth' => [
+                '', ''
+            ]
+        ]);
+
+        die(var_dump($response));
+    }
+}
---
+++ src/app.php
@@ -0,0 +1,25 @@
+<?php
+
+use JiraAPI\APICallerServiceProvider;
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+$app["api_caller_service_provider"] = function() use ($app) {
+    return new APICallerServiceProvider();
+};
+
+return $app;
