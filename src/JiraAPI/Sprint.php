349ff96a1bfc26c56a9c7ab1d1263f64a7e84adb
DeRidStijn <deridder.stijn93@gmail.com>
2018-05-07 10:22:17 +0200

    made a Sprint class that will contain a list off issues

--- src/JiraAPI/Sprint.php
+++ src/JiraAPI/Sprint.php
@@ -11,5 +11,82 @@ namespace JiraAPI;
 
 class Sprint
 {
+    private $name;
+    private $goal;
+    private $id;
+    private $issues = [];
+
+    public function __construct(string $name, string $goal, int $id, array $issues)
+    {
+        $this->name = $name;
+        $this->goal = $goal;
+        $this->id = $id;
+        $this->issues = $issues;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) : string
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoal() : string
+    {
+        return $this->goal;
+    }
+
+    /**
+     * @param string $goal
+     */
+    public function setGoal($goal) : string
+    {
+        $this->goal = $goal;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id) : int
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIssues() : array
+    {
+        return $this->issues;
+    }
+
+    /**
+     * @param array $issues
+     */
+    public function setIssues($issues) : array
+    {
+        $this->issues = $issues;
+    }
+
 
 }
