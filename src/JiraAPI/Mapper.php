cd52e12bcbea7d666ca87eb2006e701824d45c09
DeRidStijn <deridder.stijn93@gmail.com>
2018-05-07 14:46:33 +0200

    test for mapper class to make sprint and issues

---
+++ src/JiraAPI/Mapper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deridstijn
+ * Date: 5/7/18
+ * Time: 1:04 PM
+ */
+
+namespace JiraAPI;
+
+
+class Mapper
+{
+
+}
---
+++ tests/MapperTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use JiraAPI\Mapper;
+
+class MapperTest extends \PHPUnit\Framework\TestCase
+{
+    private $mapper;
+    private $sprintArray =[];
+    public function setUp()
+    {
+        $this->sprintArray = [
+            'sprintName' => 'SPRINT 16-4',
+            'goal' => 'the moon',
+            'sprintId' => 485,
+            'issues' => [
+                'issues' => [''
+                   => [
+                       [
+                           'key' => 'Testissue-255',
+                           'id' => 30,
+                           'fields' => [
+                               'status' => [
+                                   'statusCategory' => [
+                                       'name' => 'Waiting for validation'
+                                   ]
+                               ],
+                               [
+                                   'summary' => 'een heel cool test issue'
+                               ],
+                               [
+                                   'assignee' => [
+                                       'name' => 'Joske'
+                                   ]
+                               ]
+                           ]
+                       ],
+                        [
+                            'key' => 'Testissue-256',
+                            'id' => 40,
+                            'fields' => [
+                               'status' => [
+                                   'statusCategory' => [
+                                       'name' => 'To Do'
+                                   ]
+                               ],
+                               [
+                                   'summary' => 'een iets minder cool test issue'
+                               ],
+                               [
+                                   'assignee' => [
+                                       'name' => 'Jefke'
+                                   ]
+                               ]
+                            ]
+                        ]
+
+                    ]
+                ]
+            ]
+        ];
+
+        $this->mapper = new Mapper($this->sprintArray);
+    }
+
+
+    /**
+     * @test
+     */
+    public function mapper_can_make_a_new_sprint_from_the_sprint_array()
+    {
+        $this->mapper->makeNewSprint();
+        $result = $this->mapper->getSprintInfo();
+
+        $this->assertEquals('SPRINT 16-4', $result['sprintName']);
+        $this->assertEquals('the moon', $result['goal']);
+        $this->assertEquals('485', $result['id']);
+
+    }
+
+    /**
+     * @test
+     */
+    public function mapper_can_make_multiple_issues_from_the_sprint_array()
+    {
+        $this->mapper->makeNewIssues();
+        $issue1 = $this->mapper->getIssueById(30);
+        $issue2 = $this->mapper->getIssueById(40);
+
+        $this->assertEquals(30, $issue1['id']);
+        $this->assertEquals('Testissue-255', $issue1['key']);
+        $this->assertEquals('een heel cool test issue', $issue1['summary']);
+        $this->assertEquals('Waiting for validation', $issue1['status']);
+        $this->assertEquals('Joske', $issue1['assignee']);
+
+        $this->assertEquals(40, $issue2['id']);
+        $this->assertEquals('Testissue-256', $issue2['key']);
+        $this->assertEquals('een iets minder cool test issue', $issue2['summary']);
+        $this->assertEquals('To Do', $issue2['status']);
+        $this->assertEquals('Jefke', $issue2['assignee']);
+
+    }
+    
+}
