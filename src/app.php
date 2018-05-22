919046e3e1137f0c268bb455914e1617372c974b
DeRidStijn <deridder.stijn93@gmail.com>
2018-05-22 11:13:20 +0200

    swiftmailer toegevoegd

--- src/app.php
+++ src/app.php
@@ -11,10 +11,13 @@ use Silex\Provider\HttpFragmentServiceProvider;
 $config = require __DIR__ . '/../secrets/secrets.php';
 
 $app = new Application();
+$app->register(new Silex\Provider\RoutingServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
