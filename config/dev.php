c2d7a8d1b6af45cc94c8f761ea71ba7373d869d3
GijsPaulussen <gijs@tactics.be>
2018-10-26 16:45:15 +0200

    use datalog v2

--- config/dev.php
+++ config/dev.php
@@ -4,15 +4,13 @@ use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 
 // include the prod configuration
-require __DIR__.'/prod.php';
+require __DIR__ . '/prod.php';
 
 // enable the debug mode
 $app['debug'] = true;
 
-$app->register(new MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
+$app->register(new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__ . '/../var/cache/profiler',
 ));
 
-$app->register(new WebProfilerServiceProvider(), array(
-    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
-));
+$app['monolog']->pushHandler(new \Datalog\Handler\MainHandler($app, 'php://stdout', \Monolog\Logger::DEBUG));
--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,12 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(new \Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../var/logs/silex_' . $app['env'] . '.log',
+));
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app['monolog']->pushHandler(new \Datalog\Handler\MainHandler($app, 'php://stdout'));
+$app['monolog']->pushHandler(new \Datalog\Handler\ApplicationHandler($app));
